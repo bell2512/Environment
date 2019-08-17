09bd8412f01d2893ac996785fadfada488ef7c02
anh135 <nquanganh135@gmail.com>
2019-08-18 02:40:01 +0700

    erffdf

--- stories.js
+++ stories.js
@@ -12,3 +12,19 @@ var clickImg3 = document.getElementById('text-image3');
 clickImg3.addEventListener('click', function() {
     window.location = "./n3.html"
 })
+
+var clickImg4 = document.getElementById('text-image4');
+clickImg4.addEventListener('click', function() {
+    window.location = "./n4.html"
+})
+
+var clickImg5 = document.getElementById('text-image5');
+clickImg5.addEventListener('click', function() {
+    window.location = "./n5.html"
+})
+
+var clickImg6 = document.getElementById('text-image6');
+clickImg6.addEventListener('click', function() {
+    window.location = "./n6.html"
+})
+
