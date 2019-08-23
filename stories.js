0942ef6bc28cc02d51b89eef499b0148974e2450
anh135 <nquanganh135@gmail.com>
2019-08-23 18:52:55 +0700

    edaas

--- stories.js
+++ stories.js
@@ -1,30 +1,30 @@
-var clickImg1 = document.getElementById('text-image1');
-clickImg1.addEventListener('click', function () {
-    window.location = "./n1.html"
-})  
+// var clickImg1 = document.getElementById('text-image1');
+// clickImg1.addEventListener('click', function () {
+//     window.location = "./n1.html"
+// })  
 
-var clickImg2 = document.getElementById('text-image2');
-clickImg2.addEventListener('click', function() {
-    window.location = "./n2.html"
-})
+// var clickImg2 = document.getElementById('text-image2');
+// clickImg2.addEventListener('click', function() {
+//     window.location = "./n2.html"
+// })
 
-var clickImg3 = document.getElementById('text-image3');
-clickImg3.addEventListener('click', function() {
-    window.location = "./n3.html"
-})
+// var clickImg3 = document.getElementById('text-image3');
+// clickImg3.addEventListener('click', function() {
+//     window.location = "./n3.html"
+// })
 
-var clickImg4 = document.getElementById('text-image4');
-clickImg4.addEventListener('click', function() {
-    window.location = "./n4.html"
-})
+// var clickImg4 = document.getElementById('text-image4');
+// clickImg4.addEventListener('click', function() {
+//     window.location = "./n4.html"
+// })
 
-var clickImg5 = document.getElementById('text-image5');
-clickImg5.addEventListener('click', function() {
-    window.location = "./n5.html"
-})
+// var clickImg5 = document.getElementById('text-image5');
+// clickImg5.addEventListener('click', function() {
+//     window.location = "./n5.html"
+// })
 
-var clickImg6 = document.getElementById('text-image6');
-clickImg6.addEventListener('click', function() {
-    window.location = "./n6.html"
-})
+// var clickImg6 = document.getElementById('text-image6');
+// clickImg6.addEventListener('click', function() {
+//     window.location = "./n6.html"
+// })
 
